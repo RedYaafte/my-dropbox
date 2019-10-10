8f6c8767abd57802670e5a2a70401d4b57e9e098
RedYaafte <redyafte@gmail.com>
2019-10-10 17:58:17 -0500

    Added box model

--- boxes/models.py
+++ boxes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Box(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    thing = models.FileField(upload_to='uploads/', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
