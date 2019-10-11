86b9bb001930e9fced9f5c2f226bd213169610ac
RedYaafte <redyafte@gmail.com>
2019-10-11 14:49:38 -0500

    Added BoxesViewSet

--- boxes/views.py
+++ boxes/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+
+from .models import Box
+from .serializers import BoxSerializers
+
+
+class BoxViewSet(ModelViewSet):
+    queryset = Box.objects.all()
+    serializer_class = BoxSerializers
+
+
+# @api_view(['GET'])
+# def get_box_list(request):
+#     queryset = Box.objects.all()
+#     return Response({"Hola": "Mundo"}, status=status.HTTP_200_OK)
