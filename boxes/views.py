5b046fd7a7a397504af4b7f6f243a556f0a0d753
redyaafte <redyafte@gmail.com>
2019-10-12 15:21:30 -0500

    Added box_list and box_detail

--- boxes/views.py
+++ boxes/views.py
@@ -9,12 +9,28 @@ from .models import Box
 from .serializers import BoxSerializers
 
 
-class BoxViewSet(ModelViewSet):
-    queryset = Box.objects.all()
-    serializer_class = BoxSerializers
+@api_view(['GET', 'POST'])
+def box_list(request):
+    if request.method == 'GET':
+        boxes = Box.objects.all()
+        serializer = BoxSerializers(boxes, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BoxSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['GET'])
-# def get_box_list(request):
-#     queryset = Box.objects.all()
-#     return Response({"Hola": "Mundo"}, status=status.HTTP_200_OK)
+@api_view(['GET', 'PUT', 'DELETE'])
+def box_detail(request, pk):
+    try:
+        box = Box.objects.get(pk=pk)
+    except Box.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BoxSerializers(box)
+        return Response(serializer.data)
