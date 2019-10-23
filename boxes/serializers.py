642ded70a2e6640fbacec21ebcbeb88f27dddd2c
RedYaafte <redyafte@gmail.com>
2019-10-23 18:11:07 -0500

    Added update

--- boxes/serializers.py
+++ boxes/serializers.py
@@ -4,9 +4,17 @@ from .models import Box
 
 
 class BoxSerializers(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     description = serializers.CharField(required=False)
     thing = serializers.FileField(required=False)
 
     def create(self, validated_data):
         return Box.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.thing = validated_data.get('thing', instance.thing)
+        instance.save()
+        return instance
